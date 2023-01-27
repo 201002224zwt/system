428391a3a3db40926399896b6eaa0245943f4f2b
阿中中中中 <1814463167@qq.com>
2023-01-27 15:15:06 +0800

    2

--- src/main.js
+++ src/main.js
@@ -2,9 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css'
-
+import axios from 'axios';
 Vue.config.productionTip = false
 Vue.use(ElementUI);
+Vue.prototype.$axios = axios//全局
 //dasddasdsa
 new Vue({
   el:'#app',
