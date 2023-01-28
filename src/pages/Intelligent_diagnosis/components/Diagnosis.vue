18b523ff59123623cb34852ff7a5afec3b893173
阿中中中中 <1814463167@qq.com>
2023-01-28 11:32:01 +0800

    3

--- src/pages/Intelligent_diagnosis/components/Diagnosis.vue
+++ src/pages/Intelligent_diagnosis/components/Diagnosis.vue
@@ -39,6 +39,7 @@
             <el-button @click="clear">清空</el-button>
           </el-form-item>
         </el-form>
+
       </el-card>
     </div>
     <!--    症状描述和图片上传模块-->
@@ -56,14 +57,14 @@
         </div>
         <el-input
             type="textarea"
-            :rows="6"
+            :rows="5"
             placeholder="请输入内容"
             v-model="symptom"
             maxlength="200"
             show-word-limit style="margin-top: 10px;margin-bottom: 10px"
         >
         </el-input>
-        <el-button type="primary" round>提交</el-button>
+        <el-button type="primary" round @click="startup">提交</el-button>
       </el-card>
       <div style="justify-content: center;margin-top:10px">
         <el-upload
@@ -77,6 +78,45 @@
         </el-upload>
       </div>
     </div>
+    <el-dialog
+        :visible.sync="dialogVisible"
+        width="30%"
+        :before-close="handleClose" :fullscreen="true">
+      <div style="text-align: center;margin-bottom: 20px">
+        <span style="color: black;
+                    font-size: 30px;
+                    font-weight: 800;
+                    font-family: 华文仿宋;">
+                    诊断结果
+                </span>
+      </div>
+      <div style="text-align: center;margin-bottom:10px">
+        <el-progress type="circle" :percentage="progress"></el-progress>
+      </div>
+      <el-card class="box-card" shadow="always">
+        <div slot="header" class="clearfix">
+          <span style="font-size: 20px">症状分析</span>
+        </div>
+        <el-collapse v-for="(re,key) in result" :key="key" >
+          <el-collapse-item :title="re.illness"  >
+            <div style="font-size: 16px">原因分析:{{ re.analysis }}</div>
+          </el-collapse-item>
+        </el-collapse>
+      </el-card>
+      <div style="margin-bottom: 20px"></div>
+      <el-card class="box-card">
+        <div slot="header" class="clearfix">
+          <span style="font-size: 20px">药物推荐</span>
+        </div>
+        <div v-for="(item,index) in proscription" :key="index" class="text item">
+          {{ '药物'+(index+1)+"\t"+ item }}
+        </div>
+      </el-card>
+      <span slot="footer" class="dialog-footer">
+    <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+    </el-dialog>
   </div>
 
 
@@ -89,6 +129,13 @@ export default {
   data() {
     return {
       value1: 0,
+      symptom: "",
+      result: [//分析出来的病及其对应的分析
+        {illness: "风寒", analysis: "吹空调"},
+        {illness: "发热", analysis: "阳了"},
+        {illness: "风湿", analysis: "下雨天吹电风扇"}
+      ],
+      proscription:["六味地黄丸","人参","冬虫夏草","当归"],
       form: {
         name: '',
         personality: '',
@@ -99,12 +146,33 @@ export default {
         resource: '',
         desc: '',
       },
-      symptom: ""
+      progress: 0,
+      dialogVisible: false
     }
   },
   methods: {
     clear() {
       this.form.desc = ""
+    },
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+          .then(value => {
+            done();
+            console.log(value)
+          })
+          .catch(error => {
+            console.log(error)
+          });
+    },
+    startup() {
+      this.dialogVisible = true
+      setInterval(this.valChange, 1000);
+    },
+    valChange() {
+      this.progress += 15;
+      if (this.progress > 100)
+        this.progress = 100;
+      console.log("progress:" + this.progress)
     }
   }
 }
@@ -119,4 +187,12 @@ export default {
 .clearfix:after {
   clear: both
 }
+.text {
+    font-size: 16px;
+  }
+
+.item {
+    margin-bottom: 18px;
+  }
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,12 +3,13 @@ import Vue from 'vue'
 import HelloWorld from "../components/HelloWorld"
 import Router from 'vue-router'
 import Diagnosis from "@/pages/Intelligent_diagnosis/components/Diagnosis"
-
+import DiagnosticResult from "@/pages/Intelligent_diagnosis/components/Result"
 
 Vue.use(Router);
 export default new Router({
     routes: [
         {path:'/',name:'hello',component:HelloWorld},
         {path:'/diagnosis',name:'diagnosis',component:Diagnosis},
+        {path:'/result',name:'result',component:DiagnosticResult},
     ]
 })
